ea1fca8520768204b4bc01d48a2d174f8a72e48b
アレクサンダー.eth <gpg@pavlovcik.com>
2023-02-19 16:56:03 +0800

    fix(format): eslint settings

--- src/pages/*.netlify.app/index.ts
+++ src/pages/*.netlify.app/index.ts
@@ -1,8 +1,8 @@
-import { Page, Browser } from 'puppeteer';
-import { log } from '../../scraper-kernel/src/logging';
+import { Page, Browser } from "puppeteer";
+import { log } from "../../scraper-kernel/src/logging";
 export default async function projectViewController(
-    browser: Browser,
-    page: Page
-  ) {
-    log.ok(`loaded ${page.url()}`)
-  }
+  browser: Browser,
+  page: Page
+) {
+  log.ok(`loaded ${page.url()}`);
+}
