e167458501e30487fecc64d64efd0a118b36b315
アレクサンダー.eth <gpg@pavlovcik.com>
2023-11-19 16:13:44 +0900

    Update database export path

--- src/utils/export-database/index.ts
+++ src/utils/export-database/index.ts
@@ -3,20 +3,21 @@ import commandLineArgs from "../../cli/cli-args";
 
 import grab from "./fetch-from-supabase";
 import writeCsvToDisk from "./json2csv";
-import { resolveProjectPath } from "../../scraper-kernel/src/boot/events/search-for-import";
+import { resolvePagesPath } from "../../scraper-kernel/src/boot/events/search-for-import";
 import { log } from "../../scraper-kernel/src/logging";
 
 const tableName = commandLineArgs.table as string;
+
+if (!tableName) {
+  throw new Error("no table name provided");
+}
 
 async function _wrapper() {
   const data = await grab(tableName, "*");
   if (data.length) {
     const headers = Object.keys(data[0]);
     const date = Date.now();
-    const filename = path.resolve(
-      resolveProjectPath(),
-      `database-export-${date}.csv`
-    );
+    const filename = path.resolve(resolvePagesPath(), `database-export-${date}.csv`);
     writeCsvToDisk(headers, data, filename);
     log.ok(`wrote ${data.length} rows to ${filename}`);
   }
